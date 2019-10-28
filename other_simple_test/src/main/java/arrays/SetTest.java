6869f76097f5d8f45e1a9d01dab6f9cfa63f56c3
yogurtzzz <1009784814@qq.com>
2019-10-28 17:17:22 +0800

    测试一下,EnumSet

--- other_simple_test/src/main/java/arrays/SetTest.java
+++ other_simple_test/src/main/java/arrays/SetTest.java
@@ -1,4 +1,29 @@
 package arrays;
 
+import org.junit.Test;
+
+import java.util.EnumSet;
+
 public class SetTest {
+    public enum COLOR{
+        BLUE("蓝色"),RED("红色"),YELLOW("黄色");
+        private String desc;
+        COLOR(String d){
+            desc = d;
+        }
+
+        @Override
+        public String toString() {
+            return this.desc;
+        }
+    }
+    @Test
+    public void test(){
+        EnumSet<COLOR> enumSet = EnumSet.noneOf(COLOR.class);
+        COLOR blue = COLOR.BLUE;
+        enumSet.add(blue);
+        for (COLOR color : enumSet){
+            System.out.println(color);
+        }
+    }
 }
