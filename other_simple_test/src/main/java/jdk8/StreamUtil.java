42389268264193556dd5129a6ca0522a474282f9
yogurtzzz <1009784814@qq.com>
2020-01-06 16:46:30 +0800

    使用stream的flatMap可以实现流的扁平化，若一个List中的每个元素内都包含了一个List，可以用来将每个元素的List，合并成一个大的List

--- other_simple_test/src/main/java/jdk8/StreamUtil.java
+++ other_simple_test/src/main/java/jdk8/StreamUtil.java
@@ -2,7 +2,9 @@ package jdk8;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,5 +15,47 @@ public class StreamUtil {
         List<String> list = Arrays.asList("China","America","Japan","Finland");
         String concat = list.stream().collect(Collectors.joining("/"));
         System.out.println(concat);
+    }
+
+    @Test
+    public void test(){
+        List<String> list = Collections.EMPTY_LIST;
+        String collect = list.stream().collect(Collectors.joining("/"));
+
+        if (list.isEmpty())
+            System.out.println("empty");
+        else
+            System.out.println("not");
+        System.out.println(collect);
+    }
+
+    /**
+     * 使用flatMap来扁平化一个stream，可以达到合并多个List的效果，如下
+     * **/
+    @Test
+    public void testFlatMap(){
+        class Yogurt{
+            private List<String> innerList;
+            public List<String> getInnerList() {
+                return innerList;
+            }
+            public void setInnerList(List<String> innerList) {
+                this.innerList = innerList;
+            }
+        }
+        List<Yogurt> yogurtList = new ArrayList<>();
+        Yogurt y1 = new Yogurt();
+        Yogurt y2 = new Yogurt();
+
+        List<String> in1 = Arrays.asList("hello","world","this","is","yogurt");
+        List<String> in2 = Arrays.asList("can","you","merge","this","?");
+        y1.setInnerList(in1);
+        y2.setInnerList(in2);
+
+        yogurtList.add(y1);
+        yogurtList.add(y2);
+
+        List<String> merge = yogurtList.stream().flatMap(v->v.getInnerList().stream()).collect(Collectors.toList());
+        merge.forEach(v-> System.out.println(v));
     }
 }
